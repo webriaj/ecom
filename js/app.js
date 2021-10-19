6479ef01b88d76ae6c968f54ab641b45738aba82
Md Riaj Khan <71939620+webriaj@users.noreply.github.com>
2021-10-19 12:56:54 +0600

    all

--- js/app.js
+++ js/app.js
@@ -18,3 +18,13 @@ function amount2(){
     var wqty =parseFloat(document.getElementById("qty").value);
     document.getElementById("price").value = (priceValue*wqty).toFixed(2);
 }
+
+
+var n = 5;
+var sum = 0;
+
+for(n==1;n>=5; n++){
+    sum += n**2
+    n*n
+}
+console.log(sum)
